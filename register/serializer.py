6ead7820f4b002597a75cbc4b0f4544e9f580710
DimasGithub <dim.dim10032000@gmail.com>
2021-10-09 01:44:26 +0700

    registration API'S to seller

--- register/serializer.py
+++ register/serializer.py
@@ -1,18 +1,44 @@
+from django.contrib.auth.models import User
 from django.db.models import fields
 from rest_framework import serializers
 from api.models import Seller
-from django.contrib.auth.models import User
 
-class RegisterSerializer(serializers.Serializer):
+# class RegistrationSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(style = {'input_type':'password'}, write_only = True)
+#     class Meta:
+#         model = Seller
+#         fields = ['seller_name', 'seller_email','seller_phone_number', 'password']
+#         extra_kwargs = {
+#             'password' : {'write_only ': True}
+#         }
+#     def save(self):
+#         account = Seller(
+#             username = self.validated_data['seller_name'], 
+#             seller_email = self.validated_data['seller_email'], 
+#             seller_phone_number = self.validated_data['seller_phone_number'])
+#         account.save()
+#         return account
+
+class UserSerializer(serializers.ModelSerializer):
+    seller_phone_number = serializers.CharField(max_length=12)
+    password = serializers.CharField(style = {'input_type':'password'}, write_only = True)
     class Meta:
         model = User
-        fields = ('username', 'password')
-        extra_kwargs = {'password': {'write_only':True}}
-    
-    def create(self, validate_data):
-        password = validate_data.pop('password')
-        user = User(**validate_data)
-        user.set_password(password)
+        fields = ['username','email', 'password', 'seller_phone_number']
+        extra_kwargs = {
+            'password' : {'write_only ': True}
+        }
+    def save(self):
+        sel = Seller(
+            seller_name = self.validated_data['username'],
+            seller_email = self.validated_data['email'],
+            seller_phone_number = self.validated_data['seller_phone_number'],
+        )
+        sel.save()
+        user = User.objects.create_user(
+            username = self.validated_data['username'], 
+            email = self.validated_data['email'],
+            password = self.validated_data['password'] )
+        user.set_password(user.password)
         user.save()
-        return user
-        
+        return user 
--- register/views.py
+++ register/views.py
@@ -1,10 +1,26 @@
+from django.http import response
 from django.shortcuts import render
 from django.contrib.auth.models import User 
-from register.serializer import RegisterSerializer
+
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-class UserCreate(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
-    permission_classes = (AllowAny,)
+from api.models import Seller
+from register.serializer import UserSerializer
+
+
+@api_view(['POST'])
+def register_view(request):
+    if request.method == 'POST':
+        userSerializer = UserSerializer(data = request.data)
+        data = {}
+        print(data)
+        if userSerializer.is_valid():
+            userSerializer.save()
+        else:
+            data = userSerializer.errors
+        return Response(data)  
+
